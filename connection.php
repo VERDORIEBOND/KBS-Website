24424389339d07a03998e2162eff7339112f1cfd
Binu-s <S1148633@student.windesheim.nl>
2019-11-15 11:48:39 +0100

    Database connectie toegevoegd. Categorieen geprint.

--- connection.php
+++ connection.php
@@ -1,1 +1,19 @@
 <?php
+
+$host = "localhost";
+$databasename = "wideworldimporters";
+$user = "root";
+$password = "";
+$port = 3306;
+
+// Create connection
+$conn = mysqli_connect($host,$user,$password,$databasename,$port);
+
+// Check connection
+if (!$conn){
+    die("Connection failed: ".mysqli_connect_error());
+}
+
+
+
+?>
--- functions.php
+++ functions.php
@@ -1,5 +1,14 @@
 <?php
 
+include "connection.php";
+
+// Print categorie namen
+$result = mysqli_query($conn,"SELECT StockGroupName FROM StockGroups");
+while($row = mysqli_fetch_assoc($result)) {
+    foreach ($row as $col => $val) {
+        echo $val. "<br>";
+    }
+}
 
 
 
