4bc9c178c8b8ce0339d858dfc0d65c00e41c4d43
Binu-s <S1148633@student.windesheim.nl>
2019-11-16 14:17:07 +0100

    Navbar links en Style.css voor navbar

--- index.php
+++ index.php
@@ -9,11 +9,31 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <title>Hello, world!</title>
 
+    <link rel="stylesheet" type="text/css" href="Style.css" media="screen" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
     <script type="text/javascript" charset="utf8" src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-2.0.3.js"></script>
 </head>
 <body>
+
+<div id="sidebar">
+    <div class="toggle-button" onclick="toggleSidebar()">
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+    <ul>
+     <li><a href="#">Airline Novelties</a></li>
+        <li><a href="#">Clothing</a></li>
+        <li><a href="#">Computing Novelties</a></li>
+        <li><a href="#">Furry Footwear</a></li>
+        <li><a href="#">Mugs</a></li>
+        <li><a href="#">Novelty Items</a></li>
+        <li><a href="#">Packaging Materials</a></li>
+        <li><a href="#">T-Shirts</a></li>
+        <li><a href="#">Toys</a></li>
+    </ul>
+</div>
 
 
 
