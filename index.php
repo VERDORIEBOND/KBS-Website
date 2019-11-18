9b2513b828f962c6cc5f49888c27d3d4569b971a
verdoriebond <blasa07325>
2019-11-18 14:02:09 +0100

    logo fix

--- index.php
+++ index.php
@@ -12,12 +12,14 @@
     <link rel="stylesheet" type="text/css" href="Style.css" media="screen" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
-    <script type="text/javascript" charset="utf8" src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-2.0.3.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 
 </head>
 <body>
 <nav class="navbar navbar-inverse navbar-expand-lg navbar-fixed-top">
     <div class="container-fluid">
+        <img src="images/WWI.png" alt="Logo" height="50"></a>
         <div class="navbar-header">
             <a class="navbar-brand" href="#">Wide World Importers</a>
         </div>
