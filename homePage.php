78e7a129055d90522281c796ac492b67daaf95b7
verdoriebond <blasa07325>
2019-11-27 14:04:11 +0100

    Mail werkt!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! en nieuwe files toegevoegd

--- homePage.php
+++ homePage.php
@@ -1,3 +1,24 @@
+<?php
+include 'HTML Emails.php';
+
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+// Load Composer's autoloader
+require 'vendor/autoload.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+
+
+?>
+
+
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -39,13 +60,39 @@ if(isset($_GET['button1']))
 {
     $email = $_GET['emailin'];
     $subject = "Je bent ingeschreven voor de nieuwsbrief!";
-    $message = "Je bent nu ingeschreven voor de nieuwsbrief, gefeliciteerd!";
     $headers = "From: sender\'s email";
-    if (mail($email, $subject, $message,$headers)) {
-        echo "Email successfully sent to $email...";
-    } else {
-        echo "Email sending failed...";
-    }
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'wideworldimporters.5@gmail.com';                     // SMTP username
+    $mail->Password   = 'aardappel';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 587;                                    // TCP port to connect to
+   //Recipients
+    $mail->setFrom('lucasdegruijter@gmail.com', 'Lucas');
+    $mail->addAddress($email);     // Add a recipient
+    //$mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    // Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = $subject;
+    $mail->Body    = $signupEmail;                                    //for html clients
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';        //for non html clients
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    //echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 }
 ?>
 
