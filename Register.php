96d1b476124ff21f03d95aaebf00055c2f135c22
Nielsivd99 <nielsvdijk@hotmail.com>
2019-11-28 17:09:05 +0100

    Query register *fix

--- Register.php
+++ Register.php
@@ -38,7 +38,7 @@ if(isset($_POST['submit'])){
         $sql = "SELECT email FROM CustomerPrivate WHERE email = $sqlemail";
         $check = mysqli_query($conn, $sql);
         if(mysqli_num_rows($check) > 0){
-            $email_err=("Dit emailadres is reeds in gebruik, heeft u al een account:". ?> <a href="#">Login</a> <?php);
+            $email_err="Dit emailadres is reeds in gebruik, heeft u al een account: <a href='#'>Login</a>";
         } else {
             $email = $_POST['email'];
         }
