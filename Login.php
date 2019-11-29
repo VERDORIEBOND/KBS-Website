16a326a3969f006e1a422c188b69ec01dde23fa5
Nielsivd99 <nielsvdijk@hotmail.com>
2019-11-29 13:03:37 +0100

    Login page register fix

--- Login.php
+++ Login.php
@@ -1,7 +1,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Sign Up</title>
+    <title>Login</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
         body{ font: 14px sans-serif; }
@@ -10,27 +10,23 @@
 </head>
 <body>
 <div class="wrapper">
-    <h2>Inloggen</h2>
+    <h2>Login</h2>
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
             <label>Username</label>
-            <input type="text" name="email" class="form-control" value="<?php echo $username; ?>" placeholder="Email">
+            <input type="text" name="email" class="form-control" value="<?php echo $username; ?>" placeholder="email">
             <span class="help-block"><?php echo $username_err; ?></span>
         </div>
         <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
             <label>Password</label>
-            <input type="password" name="password" class="form-control" value="<?php echo $password; ?>" placeholder="Wachtwoord">
+            <input type="password" name="password" class="form-control" placeholder="Wachtwoord">
             <span class="help-block"><?php echo $password_err; ?></span>
         </div>
-        <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
-            <label>Confirm Password</label>
-            <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
-            <span class="help-block"><?php echo $confirm_password_err; ?></span>
-        </div>
         <div class="form-group">
-            <input type="submit" class="btn btn-primary" value="Submit">
-            <input type="reset" class="btn btn-default" value="Reset">
+            <input type="submit" class="btn btn-primary" value="Login">
         </div>
-        <p>Already have an account? <a href="login.php">Login here</a>.</p>
+        <p>Heb je nog geen account? <a href="register.php">Sign up now</a>.</p>
     </form>
 </div>
+</body>
+</html>
