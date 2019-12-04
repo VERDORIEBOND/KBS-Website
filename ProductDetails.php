eded16647dd77d3bfd75df2dac18ad26a38e7571
Martijn <s1144017@student.windesheim.nl>
2019-12-04 19:12:10 +0100

    als je over de foto hovert zoomt hij in

--- ProductDetails.php
+++ ProductDetails.php
@@ -27,12 +27,23 @@ include "index.php";
     ?>
 </div>
 
-<div class="Productfoto">
-    <?php
-    //foto printen
-    echo '<img src="images/no-product-image.png" alt="ProductImage">';
-    ?>
-</div>
+    <div class="Productfoto">
+        <?php
+
+        //foto printen
+
+        $numFromUrl = $_GET['productId'];
+        $query = "SELECT i.StockGroupName as groupname FROM stockgroups i JOIN stockitemstockgroups g ON i.StockGroupID=g.StockGroupID WHERE g.StockItemID = '$numFromUrl' ORDER BY RAND() LIMIT 1  ;";
+        $result= mysqli_query($conn,$query);
+        while($row = mysqli_fetch_assoc($result)) {
+        ?>
+        <div class="foto">
+            <img src="<?php echo $imgCategory($row['groupname']) ?>" class= alt="a" />
+            <?php } ?>
+
+        </div>
+    </div>
+
 
 <div class="Prijs">
     <?php
@@ -76,7 +87,20 @@ include "index.php";
 
 
 <div class="Omschrijving">
-    <p>Omschrijving</p>
+    <?php
+    $NumberUrl = $_GET ['productId'];
+    $query2 = "SELECT i.StockItemID, MarketingComments FROM stockitems i WHERE StockItemID = '$NumberUrl'";
+    $result2 = mysqli_query($conn,$query2);
+    if(!empty(trim($row['MarketingComments']))) {
+        echo "Bij dit product zit geen beschrijving";
+    }
+    else{
+        $row = mysqli_fetch_assoc($result2);
+        echo $row['MarketingComments'];
+
+    }
+    ?>
+
 </div>
 <div class="TempShower"
     <?php
