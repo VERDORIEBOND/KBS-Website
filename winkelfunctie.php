84f03d9f2d683e336ed19729979be85e6c248973
verdoriebond <blasa07325>
2019-12-10 10:14:28 +0100

    Aanbieding shit

--- winkelfunctie.php
+++ winkelfunctie.php
@@ -6,7 +6,7 @@ include_once "connection.php";
 include_once "functions.php";
 include_once "index.php";
 //include_once "orderEmail.php";
-error_reporting(0);
+error_reporting(-1);
 //$InvoiceTest = new Quickshiftin\Pdf\Invoice\InvoiceTest();
 
 
